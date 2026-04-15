ff1fd056fb290bbe30f92f522f1d44c43073cd60
arnaudmaillet <arnaud.maillet.pro@gmail.com>
2026-04-16 00:30:54 +0200

    fix: add dev branch to CI/CD

--- apps/frontend/next-env.d.ts
+++ apps/frontend/next-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+import "./.next/dev/types/routes.d.ts";
 
-// eslint-disable-next-line @nx/enforce-module-boundaries
-import "./../../dist/apps/frontend/.next/types/routes.d.ts";
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
