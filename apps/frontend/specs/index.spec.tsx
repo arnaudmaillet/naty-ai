b8cd6b4d9f8d9afb8c8273e734dbb28346aac122
arnaudmaillet <arnaud.maillet.pro@gmail.com>
2026-04-16 00:33:59 +0200

    fix: add dev branch to CI/CD

--- apps/frontend/specs/index.spec.tsx
+++ apps/frontend/specs/index.spec.tsx
@@ -2,19 +2,31 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Page from '../src/app/page';
 
+// Mock de Next Navigation
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+  }),
+  useSearchParams: () => ({
+    get: jest.fn().mockReturnValue(null),
+  }),
+}));
+
+// Mock de React Markdown
+jest.mock('react-markdown', () => ({
+  __esModule: true,
+  default: ({ children }: any) => <div>{children}</div>,
+}));
+
+// Mock de Remark GFM
+jest.mock('remark-gfm', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
 describe('Page', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Page />);
     expect(baseElement).toBeTruthy();
   });
 });
-
-jest.mock('react-markdown', () => ({
-  __esModule: true,
-  default: ({ children }: { children: string }) => <div>{children}</div>,
-}));
-
-jest.mock('remark-gfm', () => ({
-  __esModule: true,
-  default: () => {},
-}));
